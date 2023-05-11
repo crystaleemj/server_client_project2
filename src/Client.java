16d122bc922be1ebea95554040c117f432b07aca
crystaleemj <ceemj_@hotmail.com>
2023-05-11 15:11:41 +0800

    first commit

---
+++ src/Client.java
@@ -0,0 +1,44 @@
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.Socket;
+
+import javax.xml.crypto.Data;
+
+public class Client {
+    public static void main (String [] args) throws Exception{
+
+        Socket sock = new Socket(args[0], Integer.parseInt(args[1]));
+
+        System.out.println("Connection Established");
+
+        File f = new File(args[2]);
+
+        String fname = f.getName();
+        long fsize = f.length();
+
+        // when reading from a file, we need file object, so we definitely need it
+        FileInputStream fis = new FileInputStream(f);
+
+        BufferedOutputStream bos = new BufferedOutputStream(sock.getOutputStream());
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        dos.writeUTF(fname);
+        dos.writeLong(fsize);
+
+        byte [] buffer = new byte [4096];
+        int bytes = 0;
+
+        while ((bytes = fis.read(buffer)) != -1){
+            bos.write(buffer, 0, bytes);
+        } 
+        bos.flush();
+        bos.close();
+
+        System.out.println(fname + " has been sent to server successfully");
+
+
+        
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,42 @@
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket serv = new ServerSocket(Integer.parseInt(args[0]));
+        System.out.println("Listening on port: " + args[0]);
+        Socket socket = serv.accept();
+        System.out.println("Connection Established");
+
+        InputStream is = socket.getInputStream();
+        DataInputStream dis = new DataInputStream(is);
+
+        String fname = dis.readUTF();
+        long fsize = dis.readLong();
+
+        FileOutputStream fos = new FileOutputStream("rcd" + File.separator + fname);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+        byte [] buffer = new byte [4096];
+        int bytes = 0;
+
+        while ((bytes = dis.read(buffer)) != -1){
+            bos.write(buffer, 0, bytes);
+        } 
+        bos.flush();
+        bos.close();
+
+        System.out.println("file " + fname + " successfully received!");
+
+
+
+
+
+    }
+}
